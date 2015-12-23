089eb70b7947ac98a7501e4ecd134ccb61318b11
Mr Heisenberg <stendertbeatz@gmail.com>
2015-12-23 17:24:37 +0100

    Minify
    Just a Small Minfiy

--- exilez/config.cpp
+++ exilez/config.cpp
@@ -1,19 +1,2 @@
-class CfgPatches {
-	class exilez {
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 0.1;
-		requiredAddons[] = {};
-	};
-};
-
-class CfgFunctions {
-	class ZOM {
-		class ZOMZ {
-			file = "exilez\init";
-			class init {
-				postInit = 1;
-			};
-		};
-	};
-};
+class CfgPatches {class exilez {units[] = {}; weapons[] = {}; requiredVersion = 0.1; requiredAddons[] = {}; }; };
+class CfgFunctions {class ZOM {class ZOMZ {file = "exilez\init"; class init {postInit = 1;}; }; }; };
