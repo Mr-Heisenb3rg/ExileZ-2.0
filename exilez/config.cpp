cf2c300edab2beedc272b782c50bc7afb20749a9
Mr Heisenberg <stendertbeatz@gmail.com>
2015-12-23 13:58:18 +0100

    Update config.cpp
    The reason for this is faster loading... when this way is been done by all codes...

--- exilez/config.cpp
+++ exilez/config.cpp
@@ -1,19 +1,15 @@
 class CfgPatches {
-	class exilez {
-		units[] = {};
-		weapons[] = {};
-		requiredVersion = 0.1;
-		requiredAddons[] = {};
-	};
-};
+class exilez {
+units[] = {};
+weapons[] = {};
+requiredVersion = 0.1;
+requiredAddons[] = {};
+}; };
 
 class CfgFunctions {
-	class ZOM {
-		class ZOMZ {
-			file = "exilez\init";
-			class init {
-				postInit = 1;
-			};
-		};
-	};
-};
+class ZOM {
+class ZOMZ {
+file = "exilez\init";
+class init {
+postInit = 1;
+}; }; }; };
